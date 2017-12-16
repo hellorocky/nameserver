b2d19dbad6e32a7a0bb3fc685d5dd465160ca395
RockyWu <hellorocky728@gmail.com>
2017-12-16 19:08:34 +0800

    first commit

---
+++ src/main/java/com/rockywu/nameserver/NameserverApplication.java
@@ -0,0 +1,12 @@
+package com.rockywu.nameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameserverApplication.class, args);
+	}
+}
