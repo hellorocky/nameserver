8c9b53073cd490d646ff2429df524c591b52ea90
RockyWu <hellorocky728@gmail.com>
2017-12-16 21:59:09 +0800

    first version

--- src/main/java/com/rockywu/nameserver/controller/NameController.java
+++ src/main/java/com/rockywu/nameserver/controller/NameController.java
@@ -1,20 +1,60 @@
 package com.rockywu.nameserver.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
-@RequestMapping("/v1/name")
+@RequestMapping(value = "/v1/name")
 public class NameController {
 
-    @RequestMapping(value = "",method = RequestMethod.GET)
-    public String GetName() {
+    @Value("${com.rockywu.username}")
+    private String username;
 
-        return "Hi, Rocky!";
+    @Value("${com.rockywu.age}")
+    private int age;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Human GetHuman() {
+
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put("username", username);
+//        map.put("hometown", hometown);
+//        map.put("age", age);
+        Human human = new Human();
+        human.setAge(age);
+        human.setUsername(username);
+
+        return human;
     }
 }
 
+
+class Human {
+    private String username;
+    private int age;
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
